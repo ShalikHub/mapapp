7121abfcd168ad7d4339f0167b18b167f363cd11
shalikram <sapkotashalikram@gmail.com>
2018-02-21 13:43:17 +0200

    initial commit

---
+++ src/Controller/MapController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class MapController extends Controller
+{
+    /**
+     * @Route("/map", name="map")
+     */
+    public function index()
+    {
+        // replace this line with your own code!
+        return $this->render('@Maker/demoPage.html.twig', [ 'path' => str_replace($this->getParameter('kernel.project_dir').'/', '', __FILE__) ]);
+    }
+}
